6480ed4e098563a7af05cf629dfac8e7a3414b09
Nico Mexis <nico.mexis@kabelmail.de>
2021-08-12 13:43:52 +0200

    Update libraries and reformat code

--- src/test/java/msplit/RuntimeCompiler.java
+++ src/test/java/msplit/RuntimeCompiler.java
@@ -1,59 +1,74 @@
 package msplit;
 
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.tree.ClassNode;
-
-import javax.tools.*;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.URI;
 import java.util.Collections;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 
-class RuntimeCompiler {
-  private RuntimeCompiler() { }
+final class RuntimeCompiler {
 
-  static Class<?> compileToClass(String className, String source) {
-    return defineClass(className, compileToBytes(className, source));
-  }
+    private RuntimeCompiler() {
+        throw new UnsupportedOperationException();
+    }
 
-  static Class<?> defineClass(String className, byte[] compiled) {
-    return new AddClassClassLoader().addClass(className, compiled);
-  }
+    static Class<?> compileToClass(String className, String source) {
+        return defineClass(className, compileToBytes(className, source));
+    }
 
-  static byte[] compileToBytes(String className, String source) {
-    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-    // Streams for output and error
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    ByteArrayOutputStream err = new ByteArrayOutputStream();
-    // Source and target files
-    JavaFileObject sourceFile = new SimpleJavaFileObject(
-        URI.create("string:///" + className + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE) {
-      @Override
-      public CharSequence getCharContent(boolean ignoreEncodingErrors) { return source; }
-    };
-    JavaFileObject targetFile = new SimpleJavaFileObject(
-        URI.create("string:///" + className + JavaFileObject.Kind.CLASS.extension), JavaFileObject.Kind.CLASS) {
-      @Override
-      public OutputStream openOutputStream() { return out; }
-    };
-    // The file manager, but returning our target file for output
-    JavaFileManager fileManager = new ForwardingJavaFileManager<StandardJavaFileManager>(
-        compiler.getStandardFileManager(null, null, null)) {
-      @Override
-      public JavaFileObject getJavaFileForOutput(Location location, String className,
-          JavaFileObject.Kind kind, FileObject sibling) {
-        return targetFile;
-      }
-    };
-    // Run compiler
-    JavaCompiler.CompilationTask task = compiler.getTask(new OutputStreamWriter(err),
-        fileManager, null, null, null, Collections.singleton(sourceFile));
-    if (!task.call()) throw new RuntimeException("Compilation failed, output:\n" + new String(err.toByteArray()));
-    return out.toByteArray();
-  }
+    static Class<?> defineClass(String className, byte[] compiled) {
+        return new AddClassClassLoader().addClass(className, compiled);
+    }
 
-  static class AddClassClassLoader extends ClassLoader {
-    Class<?> addClass(String className, byte[] bytes) { return defineClass(className, bytes, 0, bytes.length); }
-  }
+    static byte[] compileToBytes(String className, String source) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        // Streams for output and error
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayOutputStream err = new ByteArrayOutputStream();
+        // Source and target files
+        JavaFileObject sourceFile = new SimpleJavaFileObject(
+                URI.create("string:///" + className + JavaFileObject.Kind.SOURCE.extension),
+                JavaFileObject.Kind.SOURCE) {
+            @Override
+            public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+                return source;
+            }
+        };
+        JavaFileObject targetFile = new SimpleJavaFileObject(
+                URI.create("string:///" + className + JavaFileObject.Kind.CLASS.extension), JavaFileObject.Kind.CLASS) {
+            @Override
+            public OutputStream openOutputStream() {
+                return out;
+            }
+        };
+        // The file manager, but returning our target file for output
+        JavaFileManager fileManager = new ForwardingJavaFileManager<StandardJavaFileManager>(
+                compiler.getStandardFileManager(null, null, null)) {
+            @Override
+            public JavaFileObject getJavaFileForOutput(Location location, String className,
+                                                       JavaFileObject.Kind kind, FileObject sibling) {
+                return targetFile;
+            }
+        };
+        // Run compiler
+        JavaCompiler.CompilationTask task = compiler.getTask(new OutputStreamWriter(err),
+                fileManager, null, null, null, Collections.singleton(sourceFile));
+        if (!task.call()) throw new RuntimeException("Compilation failed, output:\n" + err);
+        return out.toByteArray();
+    }
+
+    static class AddClassClassLoader extends ClassLoader {
+        Class<?> addClass(String className, byte[] bytes) {
+            return defineClass(className, bytes, 0, bytes.length);
+        }
+    }
+
 }
